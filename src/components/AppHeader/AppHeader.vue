30ab861b1a9fabe91caa0fbadc40f3fd3ac0e39e
Justin Shilling <loveofmusic25@hotmail.com>
2024-08-25 20:12:52 -0400

    add AppHeader scaffold

---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// export type PropTypes = {};
+
+// withDefaults(defineProps<PropTypes>(), {});
+</script>
+
+<template>
+  <header class="app-header">
+    <slot></slot>
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
