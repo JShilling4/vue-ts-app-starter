5beb4dbbf2a1ba7dc7ab2eb4502d348735881dfd
Justin Shilling <loveofmusic25@hotmail.com>
2024-08-25 20:12:28 -0400

    add AppDrawer scaffold

---
+++ src/components/AppDrawer/AppDrawer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+export type PropTypes = {
+  position?: "left" | "right";
+  width?: string;
+};
+
+withDefaults(defineProps<PropTypes>(), {
+  position: "left",
+  width: "200",
+});
+</script>
+
+<template>
+  <div class="app-drawer">
+    <aside>
+      <slot></slot>
+    </aside>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
