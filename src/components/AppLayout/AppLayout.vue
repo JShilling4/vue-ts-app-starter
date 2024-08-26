622b23bd99bae4d8a3fa3266e27043ba8528c379
Justin Shilling <loveofmusic25@hotmail.com>
2024-08-25 20:12:43 -0400

    add AppLayout scaffold

---
+++ src/components/AppLayout/AppLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { AppDrawer } from "../AppDrawer";
+import { AppHeader } from "../AppHeader";
+</script>
+
+<template>
+  <div class="app-layout">
+    <AppHeader>App Header</AppHeader>
+    <AppDrawer>App Drawer</AppDrawer>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app-layout {
+  position: relative;
+  width: 100%;
+  min-height: 100%;
+}
+</style>
